c2f30dafc78eb2ab54fd7879b3ca7188fa3aa31c
pbiro03 <p.biro03@stud.uni-obuda.hu>
2025-11-01 13:14:38 +0100

    changing secret keys to safe version

--- leafroute/settings.py
+++ leafroute/settings.py
@@ -12,16 +12,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.join(BASE_DIR,"leafroute")
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m#1f2m2i)#ob$h^1-8ki0#w31ab@x%9v=*y!_2jn=+dlc-%xog'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
